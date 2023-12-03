5553b5737e825e614d53ca319dcf0be62f838f49
Jonathan Lagneaux <lagneaux.j@gmail.com>
2023-12-03 19:48:35 +0100

    add day 1,2,3

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,32 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+  fun part2(input: List<String>): Int =
+    input.map {
+      it.replace("twone", "21")
+        .replace("threeight", "38")
+        .replace("nineight", "98")
+        .replace("sevenine", "79")
+        .replace("fiveight", "58")
+        .replace("eightwo", "82")
+        .replace("eighthree", "83")
+        .replace("oneight", "18")
+        .replace("one", "1")
+        .replace("two", "2")
+        .replace("three", "3")
+        .replace("four", "4")
+        .replace("five", "5")
+        .replace("six", "6")
+        .replace("seven", "7")
+        .replace("eight", "8")
+        .replace("nine", "9")
+    }.sumOf { line ->
+      val firstDigit = line.first(Char::isDigit)
+      val lastDigit = line.last(Char::isDigit)
+      "$firstDigit$lastDigit".toInt()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+  val testInput = readInput("Day01_test")
+  val testActual = part2(testInput)
+  check(testActual == 281)
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+  part2(readInput("day01")).println()
 }
---
+++ src/Day03.kt
@@ -0,0 +1,77 @@
+fun main() {
+  fun part1(input: List<String>): Int {
+    val parts = mutableListOf<Part>()
+    val symbol = mutableListOf<Symbol>()
+    input.withIndex().forEach { (index, line) ->
+      Regex("\\d+").findAll(line).forEach { matchResult ->
+        parts.add(Part(matchResult.value.toInt(), PartPosition(index, matchResult.range)))
+      }
+      Regex("[^\\w\\s.]").findAll(line).forEach { matchResult ->
+        symbol.add(Symbol(matchResult.range.first, index))
+      }
+    }
+
+    var sumOfPart = 0
+    parts.forEach { part ->
+      val startIndexBufferZone = if (part.position.y.first == 0) 0 else part.position.y.first - 1
+      val endIndexBufferZone = part.position.y.last + 1
+      val topIndexBufferZone = if (part.position.x == 0) 0 else part.position.x - 1
+      val bottomIndexBufferZone = part.position.x + 1
+
+      symbol.forEach { sym ->
+        if (sym.x in startIndexBufferZone..endIndexBufferZone && sym.y in topIndexBufferZone..bottomIndexBufferZone) {
+          sumOfPart += part.value
+        }
+      }
+    }
+    return sumOfPart
+  }
+
+  fun part2(input: List<String>): Int {
+    val parts = mutableListOf<Part>()
+    val gears = mutableListOf<Symbol>()
+    input.withIndex().forEach { (index, line) ->
+      Regex("\\d+").findAll(line).forEach { matchResult ->
+        parts.add(Part(matchResult.value.toInt(), PartPosition(index, matchResult.range)))
+      }
+      Regex("\\*").findAll(line).forEach { matchResult ->
+        gears.add(Symbol(matchResult.range.first, index))
+      }
+    }
+
+    var sumOfGear = 0
+    println(gears)
+    println(parts)
+    gears.forEach { gear ->
+      val startIndexBufferZone = gear.x - 1
+      val endIndexBufferZone = gear.x + 1
+      val topIndexBufferZone = gear.y - 1
+      val bottomIndexBufferZone = gear.y + 1
+
+      val partsInGearZone = parts.filter { part ->
+        part.position.x in topIndexBufferZone..bottomIndexBufferZone && (part.position.y.first in startIndexBufferZone..endIndexBufferZone || part.position.y.last in startIndexBufferZone..endIndexBufferZone)
+      }
+      if (partsInGearZone.size == 2) {
+        println(partsInGearZone)
+        val gearValue = partsInGearZone[0].value * partsInGearZone[1].value
+        sumOfGear += gearValue
+      }
+    }
+
+    return sumOfGear
+  }
+
+  val testInput = readInput("day_03_test")
+  val testActual = part1(testInput)
+  check(testActual == 4361) { "Expected 4361, got $testActual" }
+
+  val testActual2 = part2(testInput)
+  check(testActual2 == 467835) { "Expected 467835, got $testActual2" }
+
+  part1(readInput("day03")).println()
+  part2(readInput("day03")).println()
+}
+
+data class PartPosition(val x: Int, val y: IntRange)
+data class Part(val value: Int, val position: PartPosition)
+data class Symbol(val x: Int, val y: Int)
